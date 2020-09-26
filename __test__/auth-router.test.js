22dd0b2badcfe3a13a8db40a8751753e9b9483c9
Chris Bortel <bortelcc19@gmail.com>
2020-09-25 17:27:03 -0700

    Made somemore 'buckets' for my tests

--- __test__/auth-router.test.js
+++ __test__/auth-router.test.js
@@ -27,4 +27,25 @@ describe('Post /signup should add a new user', () => {
   });
 });
 
-describe('Test v1 route', () => {});
+// V1 (Unauthenticated API) routes
+describe(' V1 (unprotected routes) should have all of the CRUD functionality ', () => {
+  it(' POST /api/v1/todo adds an item to the DB and returns an object with the added item ', async () => {
+    // let test = testing;
+  });
+
+  it(' GET /api/v1/todo returns a list of todo items ', async () => {
+    // let test = testing;
+  });
+
+  it(' GET /api/v1/todo/ID returns a single item by ID ', async () => {
+    // let test = testing;
+  });
+
+  it(' PUT /api/v1/todo/ID returns a single, updated item by ID ', async () => {
+    // let test = testing;
+  });
+
+  it(' DELETE /api/v1/todo/ID returns an empty object. Subsequent GET for the same ID should result in nothing found', async () => {
+    // let test = testing;
+  });
+});
